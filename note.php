9a28353b1fefa5738c1ac9eed1cf35dc95f932f3
HungPham <pmhung07@gmail.com>
2017-04-19 11:18:44 +0700

    Ok Fine!

--- note.php
+++ note.php
@@ -1,2 +1,16 @@
-GIT
+GIT khởi tạo
 1. git init : Khởi tạo
+2. git add . : Đẩy file vào git trên Local
+3. git commit -m 'mes' : Bắn lên local - mes
+4. git remote add origin https://github.com/pmhung07/product.store.git (Cho minh)
+5. git push -u origin master 
+
+TẠO NHÁNH DEV
+
+1. git checkout -b tênnhánh master
+
+git merge --no-ff : Merge nhánh vào nhánh mình đang đứng
+git checkout tennhanh : chuyen doi giưa cac nhánh
+
+
+git pull origin dev
