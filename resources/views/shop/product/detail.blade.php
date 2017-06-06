bbb2abcdeddb9a092d7969a0bdd6dc55576af0ab
Justin <cong.itsoft@gmail.com>
2017-06-06 20:05:23 +0700

    detail

--- resources/views/shop/product/detail.blade.php
+++ resources/views/shop/product/detail.blade.php
@@ -67,7 +67,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=442754092739471";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=442754092739471";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
