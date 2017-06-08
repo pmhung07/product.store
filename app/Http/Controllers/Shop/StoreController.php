900b6903065671f07e564ed04dac54be8a56b4d6
Justin <cong.itsoft@gmail.com>
2017-06-08 16:38:34 +0700

    store

--- app/Http/Controllers/Shop/StoreController.php
+++ app/Http/Controllers/Shop/StoreController.php
@@ -15,13 +15,29 @@ class StoreController extends ShopController
     {
         $stores = Store::orderBy('name', 'ASC')->get();
         $products = Product::where('parent_id', 0)->orderBy('updated_at', 'DESC')->take(5)->get();
-        return view('shop/store/index', compact('stores', 'products'));
+
+        // Metadata
+        $this->metadata->title = 'Hệ thống cửa hàng';
+        $this->metadata->description = 'Hệ thống cửa hàng';
+        $this->metadata->image = $this->setting->logo ? url(parse_image_url($this->setting->logo)) : '';
+        $this->metadata->url = route('shop.store.index');
+        $metadata = $this->metadata->toArray();
+
+        return view('shop/store/index', compact('stores', 'products', 'metadata'));
     }
 
     public function getDetail($id)
     {
         $store = Store::findOrFail($id);
         $products = Product::where('parent_id', 0)->orderBy('updated_at', 'DESC')->take(5)->get();
-        return view('shop/store/detail', compact('store', 'products'));
+
+        // Metadata
+        $this->metadata->title = $store->name;
+        $this->metadata->description = $store->name;
+        $this->metadata->image = $this->setting->logo ? url(parse_image_url($this->setting->logo)) : '';
+        $this->metadata->url = route('shop.store.detail', $id);
+        $metadata = $this->metadata->toArray();
+
+        return view('shop/store/detail', compact('store', 'products', 'metadata'));
     }
 }
