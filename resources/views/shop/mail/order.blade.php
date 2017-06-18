3a4f373e22b4c7ae0b6652db9ff6b88c5d3d504e
Justin <cong.itsoft@gmail.com>
2017-06-18 18:57:04 +0700

    send mail success order

--- resources/views/shop/mail/order.blade.php
+++ resources/views/shop/mail/order.blade.php
@@ -6,11 +6,11 @@
 <body style="font-family: tahoma, arial, sans-serif">
     <h1 style="text-transform: uppercase; text-align: center;">Thông tin đơn hàng</h1>
     <div style="text-align: center;">
-        <p>Chào {{ $order->getCustomerEmail() }}!</p>
+        <p>Chào {{ $order->customers->email }}!</p>
         <p>Cảm ơn bạn đã sử dụng dịch vụ của <a href="http://{{ $_SERVER['SERVER_NAME'] }}/">{{ $_SERVER['SERVER_NAME'] }}</a></p>
         <p>Sau đây là thông tin đơn hàng của bạn</p>
     </div>
 
-    @include('frontend/email/partials/content')
+    @include('shop/mail/partials/content')
 </body>
 </html>
