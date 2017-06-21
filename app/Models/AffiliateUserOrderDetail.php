cb66133f81d92abac30ba23ae739003b9401b805
Justin <cong.itsoft@gmail.com>
2017-06-21 08:11:31 +0700

    luu don affiliate

---
+++ app/Models/AffiliateUserOrderDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliateUserOrderDetail extends Model
+{
+    protected $table = 'affiliate_user_order_detail_logs';
+    protected $guarded = ['id'];
+}
